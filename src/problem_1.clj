1b7625d76c387f7794aec29b2996c3b8da0388d0
Alejandro Herrera <alejandroherrera@Alejandros-Mac-mini.local>
2023-07-03 23:32:30 -0500

    Problem 1

---
+++ src/problem_1.clj
@@ -0,0 +1,49 @@
+(ns problem-1
+  (:require [clojure.edn :as edn]))
+
+(def invoice (edn/read-string (slurp "invoice.edn")))
+
+(defn
+  right-tax?
+  [tax-item]
+  (->> (= 19 (:tax/rate tax-item))
+       (and (= :iva (:tax/category tax-item)))))
+
+(defn
+  right-retention?
+  [retention-item]
+  (->> (= 1 (:retention/rate retention-item))
+       (and (= :ret_fuente (:retention/category retention-item)))))
+
+(defn
+  has-right-tax?
+  [invoice-item]
+  (->> (:taxable/taxes invoice-item)
+       (filter right-tax?)
+       (count)
+       (<= 1)))
+
+(defn
+  has-right-retention?
+  [invoice-item]
+  (->> (:retentionable/retentions invoice-item)
+       (filter right-retention?)
+       (count)
+       (<= 1)))
+
+(defn
+  is-correct-item?
+  [invoice-item]
+  (let [has-right-retention (has-right-retention? invoice-item)
+        has-right-tax (has-right-tax? invoice-item)]
+    (or (and has-right-retention (not has-right-tax)) (and has-right-tax (not has-right-retention)))))
+
+(defn
+  filter-invoice
+  "Filters invoice items array"
+  [invoice]
+  (->> (:invoice/items invoice)
+       (filter is-correct-item?)))
+
+(println (vector (filter-invoice invoice)))
+
